cd54cacff58bff5f41569a905565a9330b32b30e
Nayem1503019 <106160241+Nayem1503019@users.noreply.github.com>
2023-08-08 11:54:54 +0600

    Add files via upload

---
+++ Binary exponentiation non-recursive implementation simplfied.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int binpow(int a,int n)
+{
+    int result=1;
+    while(n)
+    {
+        if(n&1)///if n&1==1, then n is odd otherwise even.
+
+           result*=a;
+        a*=a;
+        n>>=1;/// n is being divided by 2;
+        }
+
+    return result;
+}
+
+int main()
+{
+    int a,n;
+    while(scanf("%d%d",&a,&n)==2)
+    {
+        printf("%d\n",binpow(a,n));
+    }
+    return 0;
+}
+
